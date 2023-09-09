1cb2f7e903a8a571610a73e4ae4dda320f0aab23
jonas <jonas_hoerl@web.de>
2023-09-09 19:29:45 +0200

    enable adventure cards

--- scry_fall.py
+++ scry_fall.py
@@ -7,8 +7,11 @@ class ScryFall:
 
     @staticmethod
     def request_img_urls(card_name):
+        print(ScryFall.__create_params(card_name))
         response = requests.get(url=URL, params=ScryFall.__create_params(card_name))
+        print(response)
         card = response.json()["data"][0]
+        print(card)
         img_urls = ScryFall.get_img_urls(card)
         return {
             "img": img_urls["border_crop"],
@@ -17,10 +20,16 @@ class ScryFall:
 
     @staticmethod
     def get_img_urls(card):
+        if ScryFall.is_adventure(card):
+            return card["image_uris"]
         if ScryFall.is_double_faced(card):
             return card["card_faces"][0]["image_uris"]
         else:
             return card["image_uris"]
+
+    @staticmethod
+    def is_adventure(card):
+        return card["layout"] == "adventure"
 
     @staticmethod
     def is_double_faced(card):
